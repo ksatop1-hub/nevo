aed88159ff03e664152bbb40d4cb36f5a399e2c5
ksatop1-hub <ksa99099com@gmail.com>
2026-01-30 19:29:08 +0300

    Add files via upload

---
+++ asset_map.js
@@ -0,0 +1,152 @@
+// Asset Map (Read-Only)
+// Edit assets in the Media tab.
+// Use these IDs alongside lib to reference assets in your code:
+//
+// Example: lib.getAsset["player_sprite"]
+
+const assets = {
+  "najih_character_happy": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/7d3bb7d6-e4a3-4e49-8b5f-3f120505b6cb.webp",
+    "type": "image",
+    "aspect_ratio": [
+      721,
+      1448
+    ]
+  },
+  "najih_character_neutral": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/0aeb2582-012b-4dd7-b748-8dc5d38a75c4.webp",
+    "type": "image",
+    "aspect_ratio": [
+      409,
+      1243
+    ]
+  },
+  "rasib_character_worried": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/e8925924-9a1d-42d6-a836-63ce6a3f190e.webp",
+    "type": "image",
+    "aspect_ratio": [
+      531,
+      1241
+    ]
+  },
+  "rasib_character_trying": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/c2cf3f19-4550-4c33-a032-ed118be4ebdd.webp",
+    "type": "image",
+    "aspect_ratio": [
+      480,
+      1248
+    ]
+  },
+  "teacher_character": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/b65c7fa9-22aa-416d-9f92-232506bfb14f.webp",
+    "type": "image",
+    "aspect_ratio": [
+      645,
+      1241
+    ]
+  },
+  "classroom_background": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/1ab736f4-0c9a-4a75-9127-3e27588dc93b.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1536,
+      1024
+    ]
+  },
+  "chapter_map_background": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/1cc9a06e-81ff-4708-ae21-94d51f0316b8.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1536,
+      1024
+    ]
+  },
+  "knowledge_gem_green": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/0c51cf19-1e63-4051-9608-f4cf0c98fb37.webp",
+    "type": "image",
+    "aspect_ratio": [
+      747,
+      815
+    ]
+  },
+  "knowledge_gem_blue": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/532395d8-b374-4228-b1aa-fd73a31778f6.webp",
+    "type": "image",
+    "aspect_ratio": [
+      778,
+      1001
+    ]
+  },
+  "knowledge_gem_gold": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/ed1f5dde-3b3f-4fff-8212-b8de40d3c6e2.webp",
+    "type": "image",
+    "aspect_ratio": [
+      778,
+      1000
+    ]
+  },
+  "star_icon_gold": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/1ffea8a6-fe5a-4003-be53-86e53994d090.webp",
+    "type": "image",
+    "aspect_ratio": [
+      776,
+      738
+    ]
+  },
+  "star_icon_gray": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/ae48cb2c-ac1e-452e-87c7-43b83d7efa1f.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1023,
+      973
+    ]
+  },
+  "decorative_pattern_border": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/002ad836-e223-4bf0-8052-d14b302a0597.webp",
+    "type": "image",
+    "aspect_ratio": [
+      1530,
+      604
+    ]
+  },
+  "success_celebration": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/206a1433-f5d4-475e-b71a-6454ae3634dc.webp",
+    "type": "image",
+    "aspect_ratio": [
+      797,
+      799
+    ]
+  },
+  "hint_icon": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/images/a95ee4e5-2cb3-4dcf-b46f-f97f24a07c43.webp",
+    "type": "image",
+    "aspect_ratio": [
+      532,
+      793
+    ]
+  },
+  "background_music_theme": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/music/10545de6-f00a-42a6-ac03-1ead2faa0b2c.mp3",
+    "type": "audio"
+  },
+  "correct_answer_sound": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/4e75f8a8-2337-4eab-85db-48dbb11223fd.mp3",
+    "type": "audio"
+  },
+  "wrong_answer_sound": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/b913a0ef-e340-4494-9857-647a12da1b4a.mp3",
+    "type": "audio"
+  },
+  "gem_collect_sound": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/941f2f6d-0672-4ed1-874a-8b7bd00af873.mp3",
+    "type": "audio"
+  },
+  "chapter_complete_sound": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/dcafa116-a15b-48fc-9dd0-164ff3e01bec.mp3",
+    "type": "audio"
+  },
+  "button_click_sound": {
+    "url": "https://d2oir5eh8rty2e.cloudfront.net/assets/sounds/effect/ef01f4d1-5fae-4534-ac55-35444b0df808.mp3",
+    "type": "audio"
+  }
+};
